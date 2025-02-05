71a81bbea7a59cd12330fcdb06ffacacc961ceef
past-hypothesis <129170254+past-hypothesis@users.noreply.github.com>
2025-02-05 13:12:10 +0700

    near_py_tool/assets/new-project-template/contract.py: fix near.input() call signature

--- near_py_tool/assets/new-project-template/contract.py
+++ near_py_tool/assets/new-project-template/contract.py
@@ -4,14 +4,14 @@ import base58
 
 @near.export
 def hello_world():
-  input_bytes = near.input(0)
+  input_bytes = near.input()
   base58_input = base58.b58encode(input_bytes).decode('ascii')
-  near.log_utf8(f"hello_world(): input(0): {input_bytes.decode('ascii')}; b58(input(0)): {base58_input}")
+  near.log_utf8(f"hello_world(): input(): {input_bytes.decode('ascii')}; b58(input(0)): {base58_input}")
   near.value_return(json.dumps({"input_length": len(input_bytes), "base58_input": base58_input}))
 
 @near.export
 def echo():
-  input_bytes = near.input(0)
+  input_bytes = near.input()
   base58_input = base58.b58encode(input_bytes).decode('ascii')
-  near.log_utf8(f"hello_world(): input(0): {input_bytes.decode('ascii')}; b58(input(0)): {base58_input}")
+  near.log_utf8(f"hello_world(): input(): {input_bytes.decode('ascii')}; b58(input(0)): {base58_input}")
   near.value_return(json.dumps({"input_length": len(input_bytes), "base58_input": base58_input}))
